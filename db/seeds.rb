d10bf7d04f2cffedf98c33026bfa4c54a0b60371
joshua <joshuamtaylor455@gmail.com>
2023-03-10 16:09:14 -0500

    add HTTP commits

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+dog = Dog.create(name: "Nova", age: 1, breed: "Golden Retriever")
+dog = Dog.create(name: "Jojo", age: 1, breed: "Golden Doodle")
+dog = Dog.create(name: "Zuki", age: 1, breed: "Australian Shepherd")
